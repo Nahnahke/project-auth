a192182bab8f5993741ae15732dc9e19529e2463
Hannah Ek <ekhannahek@gmail.com>
2023-09-13 12:43:56 +0200

    changed url in urls

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-vaipefbdcq-ew.a.run.app"
+const BASE_URL = "https://project-auth-emnka5wlzq-ey.a.run.app"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
