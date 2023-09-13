41e92c52669f183c4d35156493d1f59c605e3b7b
Hannah Ek <ekhannahek@gmail.com>
2023-09-13 12:49:35 +0200

    updated address

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://stellar-chebakia-44f98b.netlify.app/');
+  res.header('Access-Control-Allow-Origin', 'https://stellar-chebakia-44f98b.netlify.app/login');
   next();
 });
 
